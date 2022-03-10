3bb4da793cf43b2e9bd05121db46794af1c5146b
nkunic <natasa.kunic@gmail.com>
2022-03-10 16:26:14 +0100

    knjige

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './views/homepage/homepage.component';
+import { KnjigeComponent } from './views/knjige/knjige.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, SharedModule],
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, KnjigeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/views/knjige/knjige.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-knjige',
+  templateUrl: './knjige.component.html',
+  styleUrls: ['./knjige.component.scss'],
+})
+export class KnjigeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
